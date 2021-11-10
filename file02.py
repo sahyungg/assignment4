a5f8d6229123ee6fcbe1695ecdbf0d0da2f23c26
Your Name <you@example.com>
2021-11-10 18:07:05 +0800

    4th commit

--- file02.py
+++ file02.py
@@ -1,8 +1,7 @@
-price_of_apples = int(20)
-price_of_oranges = int(25)
-
 def getPrices():
+    price_of_apples = int(20)
     apples_to_buy = int(input("How many apples would you like to buy? "))
+    price_of_oranges = int(25)
     oranges_to_buy = int(input("How many oranges would you like to buy? "))
     computation = (apples_to_buy*price_of_apples) + (oranges_to_buy*price_of_oranges)
     return computation
